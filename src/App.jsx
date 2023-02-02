e69f48b78a166bda13f3b489cd945a87563ae424
mohamad masri <masri_mohamad@protonmail.com>
2023-02-02 14:34:10 +0200

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const App = () => {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+return (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
+}
+
+export default App;
